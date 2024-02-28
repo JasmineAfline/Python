5da1af908b66ae1dd7aec227004781339e1dc941
Afline Jasmine <afline47@gmail.com>
2024-02-28 21:19:05 +0300

    add country

--- user-input.py
+++ user-input.py
@@ -4,6 +4,8 @@ age = input("How old are you? ")
 
 location = input("Where are you located? ")
 
-print(f"Hello {name}, you are {age} years old and live in {location} .") 
+country = input ("Where is your country ?")
+
+print(f"Hello {name}, you are {age} years old and live in {location},your {country} ") 
  
 
